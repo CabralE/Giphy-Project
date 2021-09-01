6b725708d97728f623f74eb15ead74c344f5dcd0
ed <cabrale234@gmail.com>
2021-09-01 19:19:41 -0400

    update all

--- README.md
+++ README.md
@@ -1,30 +1,44 @@
 Ed's Giphy Project
 ___________________________________________
-
-Table of Contents
-___________________________________________
-
- 1. API
- 2. UI/Flexbox
- 3. Library
- 4. Flexbox
-
-
-
-
-API
-___________________________________________
-API : https://developers.giphy.com/
-
-
-UI/Flexbox
 ___________________________________________
 
 
-Library
+
+Technilogies Used
 ___________________________________________
 
-JQuery : https://code.jquery.com/jquery-3.3.1.min.js 
+1. HTML5
+2. CSS3
+3. JavaScript
+4. JQuery
+5. Google fonts
+6. JQuery Modal
+7. GIPHY API
 
-test
+
+Approach taken
+___________________________________________
+
+In this project, I intended to generates gifs based on the user's search. First, I used flexbox to create a 2x2 grid for my gifs. Second, I worked on generating the gifs via GIPHY's free api. Third, connected my gifs to display on each quadrent of the flexbox. Lastly, I made the flexbox grid mobile friendly at max width of 858px. 
+
+
+
+Link to my live site
+___________________________________________
+
+
+Installation instructions
+___________________________________________
+
+API Key from here: https://developers.giphy.com/
+JQuery: https://code.jquery.com/jquery-3.3.1.min.js 
+Google fonts: https://fonts.googleapis.com
+Fontawesome account: fontawesome.com
+
+
+Unsolved problems
+___________________________________________
+
+
+
 
--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
 console.log("Test")
+let userInput;
 const API_KEY = "B7PbeTmgYEau04BJhV88U9DJojAHJTba";
 const BASE_URL = "api.giphy.com/v1/gifs/search";
-// const LIMIT = 10;
-const search = "hi";
-const $img = $('#img');
+const $imgOne = $('#imgOne');
+const $imgTwo = $('#imgTwo');
+const $imgThree = $('#imgThree');
+const $imgFour = $('#imgFour');
+const $input = $('input[type="text"]')
+const $searchBar = $('#type')
 
-const promise = $.ajax({
-    url: `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=10&q=${search}`
-});
+function getImages(event){
+    event.preventDefault()
+    userInput = $input.val()
+    $.ajax({
+        url: `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=4&q=${userInput}`
+    }).then(
+        (data) => {
+            console.log(data);
+            console.log(data.data[0].images)
+            $imgOne.attr("src", data.data[0].images.downsized.url);
+            $imgTwo.attr("src", data.data[1].images.downsized.url);
+            $imgThree.attr("src", data.data[2].images.downsized.url);
+            $imgFour.attr("src", data.data[3].images.downsized.url);
+        },
+        (error) => {
+            console.log('bad request: ', error);
+        }
+    );
+    $searchBar.val("")
+} 
 
-promise.then(
-    (data) => {
-        console.log(data);
-        console.log(data.data[0].images)
-        $img.attr("src", data.data[0].images.downsized.url);
-    },
-    (error) => {
-        console.log('bad request: ', error);
-    }
-);
+$('form').on('submit', getImages);
+
+
+
+
+
+
+
+
